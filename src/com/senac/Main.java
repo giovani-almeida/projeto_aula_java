920ad0e573168d0b2bd630c33daa1a2acd8294df
giovani-almeida <giovanialmedini@hotmail.com>
2021-03-15 21:53:27 -0300

    Exercicios

--- src/com/senac/Main.java
+++ src/com/senac/Main.java
@@ -5,13 +5,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String nome = "pedro";
-
-        int idade = 17;
-
-
-        System.out.println("a idade de "+nome +" é "+ idade);
+        usuario usuario = new Usuario();
+        usuario.verificarIdade(17);
 
 
     }
-}
+
--- src/com/senac/models/Usuario.java
+++ src/com/senac/models/Usuario.java
@@ -2,7 +2,14 @@ package com.senac.models;
 
 public class Usuario {
 
-    int idade;
+    public boolean verificarIdade(int idade) {
 
+        if (Idade >= 18){
+            System.out.println("È maior de idade");
+        } else {
+            System.out.println("Não é maior de idade");
+
+        }
+    }
 
 }
